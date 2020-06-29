2afc6cc510b823783efece02006472bd022ad6f7
Danzan Ubush <danzan1994@gmail.com>
2020-06-29 01:21:39 -0400

    Portfolio 1st Demo

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Portfolio
+<a href="https://tracking.gitads.io/?repo=React-Portfolio"><img src="https://images.gitads.io/React-Portfolio" alt=“GitAds”/></a>
+
+You can support me by clicking on the above ad, it really helps me to create amazing open source projects which help the open source community. Thanks!
+
+[![HitCount](http://hits.dwyl.io/rbhatia46/React-Portfolio.svg)](http://hits.dwyl.io/rbhatia46/React-Portfolio)
+
+
+A simple SPA Portfolio template for developer/designers built with React. Use it to showcase your work, testimonials and other information to clients.
+
+## Preview
+![Preview](https://image.ibb.co/e5uBf0/Capture.png)
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Change the values in ```src/resumeData.js``` to suit your use-case.
+4. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+5. Make required changes in ```src/resumeData.js``` to suit your needs.
+
---
+++ src/resumeData.js
@@ -0,0 +1,133 @@
+let resumeData = {
+    "imagebaseurl":"https://danzan1994.github.io/",
+    "name": "Danzan Ubush",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a newly Rutgers Coding Bootcamp graduate who is eager to enter the world of web development.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/danzan-ubush-bb7290140",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/danzan1994",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am a newly Rutgers Coding Bootcamp graduate who is eager to enter the world of web development.",
+    "address":"USA",
+    "website":"http://github.com/danzan1994",
+    "education":[
+      {
+        "UniversityName":"Rutgers Coding Bootcamp",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"University at Buffalo",
+        "specialization":"Economics",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2017",
+        "Achievements":"Schussmeisters Ski Club Volunteer of the Year 2015"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Metco Landscape",
+        "specialization":"Production Administrator",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Republic National Distribution",
+        "specialization":"Sales Operations Administrator",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Vertafore Insurance Software",
+        "specialization":"Accounts Receivable",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"DISH Network",
+        "specialization":"Human Resources, Recruiter",
+        "MonthOfLeaving":"January",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"HTML/CSS"
+      },
+      {
+        "skillname":"jQuery"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"React.js"
+      },
+      {
+        "skillname":"mySQL"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Responsive Design"
+      },
+      {
+        "skillname":"Heroku"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"MusiCoda",
+        "description":"website",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
